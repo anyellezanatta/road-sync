970b10023e9653264df930b79f034cedeb1e4a6b
anyelle zanatta <anyelle.ad@gmail.com>
2024-06-05 12:36:06 +0200

    search

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,5 +1,17 @@
 class RidesController < ApplicationController
   def index
     @rides = Ride.all
+
+    origin = params[:origin].downcase
+    destination = params[:destination].downcase
+    date = Date.parse(params[:date])
+    passengers = params[:seats].to_i
+
+    sql_subquery = " lower(origin)  @@ :origin
+      AND lower(destination)  @@ :destination
+      AND DATE(date) = :date #{params[:seats].present? ? 'AND seats >= :passengers' : ''}"
+
+    @rides = @rides.where(sql_subquery, origin:, destination:, date:,
+                                        passengers:).order(:start_time)
   end
 end
