e85d28fc39d9485dd5aa8b6f09b32001c55eb4b4
Marie Auer <marieauer29@gmail.com>
2024-06-05 11:08:23 +0200

    uikit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "dashboard", to: "pages#dashboard"
+  get "uikit", to: "pages#uikit"
   resources :rides, except: [:destroy] do
     resources :bookings, only: %i[index create update] do
       resources :messages, only: %i[index]
