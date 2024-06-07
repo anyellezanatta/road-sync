53a70db2589bac66617b24b165a9fa431f2f7c4a
Marie Auer <marieauer29@gmail.com>
2024-06-07 15:11:25 +0200

    updated dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: %i[dashboard]
   def home
   end
 
   def dashboard
-    @bookings = Booking.where(user: current_user)
-    @driver_bookings = current_user.driver_bookings
+    if current_user
+      @bookings = Booking.where(user: current_user)
+      @driver_bookings = current_user.driver_bookings
+    end
   end
 end
